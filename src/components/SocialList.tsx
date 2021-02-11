f47f0579af0b2ed1a49fb746d6b64ff50e398465
Csaba Veres <vcs222@gmail.com>
2021-02-11 17:52:36 +0100

    add mail

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
+import Mail from "../assets/mail-alt.svg";
 import config from "../lib/config";
 
 export function SocialList({}) {
   return (
     <div>
+      <a
+        title="Mail"
+        href={`mailto:${config.email_account}`}
+        target="_blank"
+        rel="noopener"
+      >
+        <Mail width={24} height={24}  />
+      </a>
       {/* <a
         title="Twitter"
         href={`https://twitter.com/${config.twitter_account}`}
@@ -20,7 +29,7 @@ export function SocialList({}) {
         target="_blank"
         rel="noopener"
       >
-        <GitHub width={24} height={24} fill={"#222"} />
+        <GitHub width={24} height={24} fill={"#222"}/>
       </a>
       <style jsx>{`
         a {
